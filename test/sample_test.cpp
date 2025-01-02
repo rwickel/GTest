aefae089bafe8c5de130c7a2b626579c48290d40
rwickel <robert.wickel@gmx.de>
2025-01-02 10:16:11 +0100

    Add files via upload

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -1,13 +1,19 @@
+#include "sample.h"
 #include <gtest/gtest.h>
 
-// A simple test case
-TEST(SampleTest, BasicAssertions) {
-    EXPECT_EQ(1, 1);  // Passes
-    EXPECT_TRUE(true); // Passes
+// Test case for the add function
+TEST(AddTest, HandlesPositiveInput) {
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(10, 15), 25);
 }
 
-// Main function for GoogleTest
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST(AddTest, HandlesNegativeInput) {
+    EXPECT_EQ(add(-1, 1), 0);
+    EXPECT_EQ(add(-5, -7), -12);
+}
+
+TEST(AddTest, HandlesZeroInput) {
+    EXPECT_EQ(add(0, 0), 0);
+    EXPECT_EQ(add(0, 5), 5);
+    EXPECT_EQ(add(5, 0), 5);
 }
